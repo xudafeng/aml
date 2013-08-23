697827de8954c782df2b6c798674eae182bb5790
dafeng <dafeng.xdf@taobao.com>
2013-08-23 13:39:34 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,28 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-
+    var config = {
+        /**
+         *  导入配置文件
+         */
         pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-            '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
-            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
+
+//        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+//            '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
+//            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+//            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+//            ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        banner: '<%= pkg.name %>',
         concat: {
             dist: {
                 src: [
-                    "src/module.js"
+                    "<%= pkg.version %>/src/module.js"
                 ],
-                dest: 'bin/debug.js'
-            }
-        },
-        // 打包后压缩文件
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            base: {
-                files: {
-                    '<%= pkg.version %>/build/index-min.js': ['<%= pkg.version %>/build/index.js']
-                }
+                dest: '<%= pkg.version %>/bin/debug.js'
             }
         }
-    });
-    grunt.loadNpmTasks("grunt-contrib-concat")
-    grunt.loadNpmTasks("grunt-contrib-uglify")
-    grunt.registerTask("default",
-        ["concat", "post-concat", "uglify", "post-uglify"])
+    };
+    grunt.initConfig(config);
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.registerTask("default",["concat","uglify"])
 };
