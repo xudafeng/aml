e4262d6970a56a062fe1a3147ee7f979b6641d64
xudafeng <xudafeng@126.com>
2013-08-23 09:23:27 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,6 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        // 配置文件，参考package.json配置方式，必须设置项是
-        // name, version, author
-        // name作为gallery发布后的模块名
-        // version是版本，也是发布目录
-        // author必须是{name: "xxx", email: "xxx"}格式
+
         pkg: grunt.file.readJSON('package.json'),
         banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
             '<%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>\n' +
@@ -12,29 +8,15 @@ module.exports = function(grunt) {
             '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
             ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
 
-        // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
-        // 添加
-        kmc: {
-            options: {
-                packages: [
-                    {
-                        name: '<%= pkg.name %>',
-                        path: '../'
-                    }
+        concat: {
+            dist: {
+                src: [
+                    "src/module.js"
                 ],
-                map: [["<%= pkg.name %>/", "gallery/<%= pkg.name %>/"]]
-            },
-            main: {
-                files: [
-                    {
-                        src: "<%= pkg.version %>/index.js",
-                        dest: "<%= pkg.version %>/build/index.js"
-                    }
-                ]
+                dest: 'bin/debug.js'
             }
         },
         // 打包后压缩文件
-        // 压缩文件和入口文件一一对应
         uglify: {
             options: {
                 banner: '<%= banner %>'
@@ -46,7 +28,8 @@ module.exports = function(grunt) {
             }
         }
     });
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-kmc');
-    return grunt.registerTask('default', ['kmc', 'uglify']);
+    grunt.loadNpmTasks("grunt-contrib-concat")
+    grunt.loadNpmTasks("grunt-contrib-uglify")
+    grunt.registerTask("default",
+        ["concat", "post-concat", "uglify", "post-uglify"])
 };
