591fb1797d8ea7ae6cec2b3b8d7e4416db61e011
dafeng <dafeng.xdf@taobao.com>
2013-08-26 12:46:56 +0800

    add

--- 1.0.0/bin/aml.js
+++ 1.0.0/bin/aml.js
@@ -2,7 +2,7 @@
  * aml.js v1.0.0
  *
  * A simple asynchronous module loader with dependency management.
- * Latest build : 2013-08-26 12:44:29
+ * Latest build : 2013-08-26 12:46:49
  *
  * http://xudafeng.github.com/aml/
  * ================================================================
